ef6ba0a9f5d6da59ea2f66e46398a97f24965027
Ahmad Ali <ahmad.ali.siddiq.bukhari@gmail.com>
2021-05-27 14:48:07 +0500

    Customer: Generated Model

---
+++ db/migrate/20210527094506_create_customers.rb
@@ -0,0 +1,10 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name, null: false
+      t.text :payment_details
+
+      t.timestamps
+    end
+  end
+end
